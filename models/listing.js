a9437dc112a41a157eb7c35bb286428651191b29
Roman Sokol <sokolroman@proton.me>
2022-10-31 19:54:21 -0400

    adding some listing.js files

---
+++ models/listing.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class listing extends Model {}
+
+listing.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bed: {
+      type: DataTypes.STRING,
+    },
+    bath: {
+      type: DataTypes.STRING,
+    },
+    lot: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Project;

fc10d570332dce85ed205dfe3f3c79e5e257d8d4
Roman Sokol <sokolroman@proton.me>
2022-10-31 20:29:38 -0400

    working routes

--- controllers/api/listingRoutes.js
+++ controllers/api/listingRoutes.js
@@ -4,5 +4,11 @@ const withAuth = require('../../utils/auth');
 
 //...code
 
+router.get('/listing', (req, res, next) => {
+    console.log('router working');
+  
+    res.render('listing', { layout: 'main' });
+  });
+  
 // module.exports = router;
 module.exports = router;
